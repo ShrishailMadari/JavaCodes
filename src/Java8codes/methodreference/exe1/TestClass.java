28aeb2640cf8e2d06906ad2983e147cc9355e7da
Shrishail <shrishailmadari@gmail.com>
2024-10-18 19:19:41 +0530

    add these module files

--- src/Java8codes/methodreference/exe1/TestClass.java
+++ src/Java8codes/methodreference/exe1/TestClass.java
@@ -3,6 +3,11 @@ package Java8codes.methodreference.exe1;
 public class TestClass {
     public static void main(String[] args) {
         EmployeeInterface employeeInterface = Employee::validateEmployee;
-        employeeInterface.isValid(new Employee());
+//        validating the employee
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("admin");
+        boolean b = employeeInterface.isValid(employee);
+        System.out.println(b);
     }
 }
