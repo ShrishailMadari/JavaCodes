f16660de4d52b05070ce86a8885fdaa2652efd7b
Shrishail <shrishailmadari@gmail.com>
2024-10-21 23:18:05 +0530

    add these files

--- src/LinkedList/LengthOfLinkedList.java
+++ src/LinkedList/LengthOfLinkedList.java
@@ -3,10 +3,10 @@ package LinkedList;
 import java.util.LinkedList;
 
 class Node{
-    int data;
+    String data;
     Node nextNode;
 
-    public Node(int data) {
+    public Node(String data) {
         this.data = data;
     }
 
@@ -14,22 +14,29 @@ class Node{
 public class LengthOfLinkedList {
     public static void main(String[] args) {
         LinkedList<Object> list = new LinkedList<>();
-        Node a = new Node(200);
-        Node b = new Node(200);
-        Node c = new Node(200);
-        Node d = new Node(200);
-        Node e = new Node(200);
-        Node f = new Node(200);
+        Node a = new Node("200");
+        Node b = new Node("300");
+        Node c = new Node("400");
+        Node d = new Node("500");
+        Node e = new Node("600");
+        Node f = new Node("700");
         a.nextNode = b;
         b.nextNode = c;
         c.nextNode = d;
         d.nextNode = e;
         e.nextNode = f;
+        int length = findLength(a);
+        System.out.println(length);
+    }
 
-        for (int i=0; i<=5; i++){
-            int num = 200;
-            list.add(num);
-            num++;
+    private static int findLength(Node head) {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data + " ");
+            temp = temp.nextNode;
+            count++;
         }
+        return count;
     }
 }
